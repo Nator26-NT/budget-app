f5dcb339599a433712be13cccecd7dca1e195bdc
Nator26-NT <111059939+Nator26-NT@users.noreply.github.com>
2022-09-25 21:30:12 +0200

    Add files via upload

---
+++ BUDGET APP.py
@@ -0,0 +1,31 @@
+
+print("BUDGET APP > ")
+
+item_1 = float(input("Enter price for item A -  "))
+item_2 = float(input("Enter price for item B -  "))
+item_3 = float(input("Enter price for item C-  "))
+item_4 = float(input("Enter price for item D -  "))
+item_5 = float(input("Enter price for item F -  "))
+
+budget_price = print(item_1 + item_2 + item_3 + item_4 + item_5)
+
+if budget_price != "10000":
+	print("U STAYED UNDER BUDGET °-° ")
+	
+elif budget_price is "11000":
+	print("u went over budget try spending less next time")
+elif budget_price is "12000":
+	print("u went over budget try spending less next time")
+elif budget_price is "13000":
+	print("u went over budget try spending less next time")
+elif budget_price is "14000":
+	print("u went over budget try spending less next time")
+elif budget_price is "15000":
+	print("u went over budget try spending less next time")
+	
+else:
+	print("Program error !!")
+
+
+
+	
